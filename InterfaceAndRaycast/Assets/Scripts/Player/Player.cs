a02ad13d7fa18cf7a061b0d0a90ceb77d0f0a0aa
Victor Matheus <victormatheus183@gmail.com>
2022-09-05 22:26:18 -0300

    Player raycast
    The player interacts with objects that is hitten by a ray that was shootten by itself and by a layermask given in the inspector

--- InterfaceAndRaycast/Assets/Scripts/Player/Player.cs
+++ InterfaceAndRaycast/Assets/Scripts/Player/Player.cs
@@ -1,36 +1,55 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private LayerMask _layerMaskToInteract;
     private Rigidbody _rigidbody;
     private PlayerMovement _playerMovement;
-
+    private PlayerRayCast _raycast;
+    private bool _canInteract;
     // Start is called before the first frame update
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
         _playerMovement = new PlayerMovement(_rigidbody);
+        _raycast = new PlayerRayCast(transform, _layerMaskToInteract);
     }
 
     // Update is called once per frame
     void Update()
     {
+        _raycast.ShootRay();
+        CheckIfPlayerCanInteract();
+    }
+
+    private void CheckIfPlayerCanInteract()
+    {
+        if (_raycast.CheckIfRayHitAnything())
+        {
+            AllowInteraction();
+        }
+    }
+    private void AllowInteraction()
+    {
         if (Input.GetKey(KeyCode.Space))
         {
-            IInteractable gameObject = GameObject.FindGameObjectWithTag("Interactable").GetComponent<IInteractable>();
-            InteractToObject(gameObject);
+            IInteractable gameObjectInteractable = _raycast.GameObjectHadHit.GetComponent<IInteractable>();
+            InteractToObject(gameObjectInteractable);
         }
+    }
+    private void InteractToObject(IInteractable gameObject)
+    {
+        gameObject.ChangeColor();
     }
     private void FixedUpdate()
     {
         _playerMovement.AllowMove();
     }
 
-    public void InteractToObject(IInteractable gameObject)
-    {
-        
-            gameObject.ChangeColor();
-    }
+
+
+
 }
